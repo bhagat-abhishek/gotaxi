5ca5293c18ee596015839e253ebc0748e18841ea
bhagat-abhishek <inst.abhishek@gmail.com>
2023-06-12 18:31:46 +0530

    Vehicle Resource - Added

---
+++ app/Filament/Resources/VehicleResource.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\VehicleResource\Pages;
+use App\Filament\Resources\VehicleResource\RelationManagers;
+use App\Models\Vehicle;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class VehicleResource extends Resource
+{
+    protected static ?string $model = Vehicle::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-truck';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+                Forms\Components\TextInput::make('name')->required(),
+                Forms\Components\TextInput::make('type')->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+                Tables\Columns\TextColumn::make('name')->searchable(),
+                Tables\Columns\TextColumn::make('type')->sortable()->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListVehicles::route('/'),
+            'create' => Pages\CreateVehicle::route('/create'),
+            'edit' => Pages\EditVehicle::route('/{record}/edit'),
+        ];
+    }    
+}
