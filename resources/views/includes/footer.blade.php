e7d1e11a6155209ce748d7c26d134411a75be224
bhagat-abhishek <inst.abhishek@gmail.com>
2023-06-12 23:26:34 +0530

    UI Layouts

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,38 @@
+   <!-- start footer Area -->
+   <footer class="footer-area section-gap">
+       <div class="container">
+           <div class="row">
+               <div class="col-lg-4 col-md-6 col-sm-6">
+                   <div class="single-footer-widget">
+                       <h6>Admin Area</h6>
+                       <ul>
+                           <li><a href="#">Admin Login</a></li>
+                       </ul>
+                   </div>
+               </div>
+               <div class="col-lg-4 col-md-6 col-sm-6">
+                   <div class="single-footer-widget mx-auto">
+                       <h6>Quick links</h6>
+                       <ul>
+                           <li><a href="#">Home</a></li>
+                           <li><a href="#">About</a></li>
+                           <li><a href="#">Service</a></li>
+                           <li><a href="#">Contact us</a></li>
+                       </ul>
+                   </div>
+               </div>
+               <div class="col-lg-4 col-md-6 col-sm-6 social-widget">
+                   <div class="single-footer-widget">
+                       <h6>Follow Us</h6>
+                       <p>Let us be social</p>
+                       <div class="footer-social d-flex align-items-center">
+                           <a href="#"><i class="fa fa-facebook"></i></a>
+                           <a href="#"><i class="fa fa-twitter"></i></a>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </div>
+       <img class="footer-bottom" src="{{ asset('assets/img/footer-bottom.png') }}" alt="">
+   </footer>
+   <!-- End footer Area -->
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,17 @@
+<header id="header">
+    <div class="header-top">
+    </div>
+    <div class="container main-menu">
+        <div class="row align-items-center justify-content-between d-flex">
+            <a href="/"><img src="{{ asset('assets/img/logo.png') }}" alt="" title="" /></a>
+            <nav id="nav-menu-container">
+                <ul class="nav-menu">
+                    <li class="menu-active"><a href="/">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact us</a></li>
+                </ul>
+            </nav><!-- #nav-menu-container -->
+        </div>
+    </div>
+</header><!-- #header -->
