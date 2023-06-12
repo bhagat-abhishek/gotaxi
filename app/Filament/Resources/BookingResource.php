12fececd75ad060a4ef23b546fd6e8837a1d4ca6
bhagat-abhishek <inst.abhishek@gmail.com>
2023-06-12 18:47:06 +0530

    Booking Resource Added

---
+++ app/Filament/Resources/BookingResource.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\BookingResource\Pages;
+use App\Filament\Resources\BookingResource\RelationManagers;
+use App\Models\Booking;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class BookingResource extends Resource
+{
+    protected static ?string $model = Booking::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-document-add';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('phone')
+                    ->tel()
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('car_type')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('pickup_location')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('drop_location')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('from')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('to')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')->searchable(),
+                Tables\Columns\TextColumn::make('phone')->searchable(),
+                Tables\Columns\TextColumn::make('car_type')->sortable()->searchable(),
+                Tables\Columns\TextColumn::make('pickup_location')->searchable(),
+                Tables\Columns\TextColumn::make('drop_location')->searchable(),
+                Tables\Columns\TextColumn::make('from'),
+                Tables\Columns\TextColumn::make('to'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListBookings::route('/'),
+            'view' => Pages\ViewBooking::route('/{record}'),
+        ];
+    }    
+}
